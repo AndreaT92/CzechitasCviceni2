23d4ee910e416128af7651c1fea1c00d303c5e5d
AndreaT92 <154465788+AndreaT92@users.noreply.github.com>
2024-05-03 19:02:33 +0200

    zkušební projekt

--- zkusebniprojekt/Program.cs
+++ zkusebniprojekt/Program.cs
@@ -1,5 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Ahoj!");
-string zkouska = Console.ReadLine();
-Console.WriteLine(zkouska);
