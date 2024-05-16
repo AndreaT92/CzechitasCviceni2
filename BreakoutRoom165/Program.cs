eaf87680ae172a0d9df58f498a16ed336e7fa946
AndreaT92 <154465788+AndreaT92@users.noreply.github.com>
2024-05-16 20:39:58 +0200

    projekt breakoutroom 16.5.

---
+++ BreakoutRoom165/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BreakoutRoom165
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            obdelnik obdelik1 = new obdelnik(5, 6);
+            obdelnik ctverec = new obdelnik(5);
+            
+            obdelik1.Vypis();
+            ctverec.Vypis();
+        }
+    }
+}
---
+++ BreakoutRoom165/obdelnik.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakoutRoom165
+{
+    public class obdelnik
+    {
+        public double Sirka;
+        public double Vyska;
+
+        public obdelnik(double sirka, double vyska)
+        {
+            Sirka = sirka;
+            Vyska = vyska;
+        }
+
+        public obdelnik(double strana): this (strana, strana)
+        {
+            
+        }
+
+        public void Vypis()
+        {
+            Console.WriteLine($" Sirka: {Sirka}, Vyska: {Vyska}");
+        }
+    }
+}
